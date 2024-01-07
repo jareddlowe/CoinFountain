812be1506e968c0790bbb55eb4130ba06a038605
jareddlowe <jareddlowe@gmail.com>
2024-01-07 06:40:20 -0500

    Update coin.gd

--- coin.gd
+++ coin.gd
@@ -5,7 +5,7 @@ var bounced_once = false
 
 func _ready():
 	# Start the first upward movement
-	$CoinSpriteBody.velocity.y -= randf_range(80, 120)
+	$CoinSpriteBody.velocity.y -= randf_range(80, 100)
 
 
 func _process(delta):
@@ -16,11 +16,12 @@ func _process(delta):
 	
 	# Apply gravity to the coin sprite's body
 	$CoinSpriteBody.velocity.y += 3
-	# Move the coin sprite's body
+
+	# Triggers when coin sprite goes below the shadow/origin point
 	if $CoinSpriteBody.global_position.y > global_position.y: 
-		$CoinSpriteBody.global_position.y = global_position.y # Prevents coin from going below shadow
-		$CoinSpriteBody.velocity.y = 0
-		if not bounced_once: # Triggers bounce if not already bounced
+		$CoinSpriteBody.global_position.y = global_position.y # Prevents coin from actually going below
+		if not bounced_once: # Triggers bounce 
 			$CoinSpriteBody.velocity.y -= randf_range(60, 80)
 			bounced_once = true
+	# Move the coin sprite's body
 	$CoinSpriteBody.move_and_slide()
